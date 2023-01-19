31de9266af7ab1c9929501bfb17467a4d44c8fda
Paula Rolim <rolim.paula1999@gmail.com>
2023-01-18 21:35:36 -0300

    feat: server - return habits

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,18 @@
 import fastify from 'fastify'
 import * as dotenv from 'dotenv'
+import { PrismaClient } from '@prisma/client'
 
 dotenv.config()
 
 const app = fastify()
 
+const prisma = new PrismaClient()
+
 const port = Number(process.env.PORT) || 3333
 
-app.get('/', () => {
-    return 'Hello world'
+app.get('/habits', async () => {
+    const habits = await prisma.habit.findMany()
+    return habits
 })
 
 app.listen({ port }).then(() => {
