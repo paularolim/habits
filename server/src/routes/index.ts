edec47a628b99a81e1108492be0d3ab13ca7f79b
Paula Rolim <rolim.paula1999@gmail.com>
2023-01-19 21:26:31 -0300

    feat: server - create habit

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,37 @@
 import { FastifyInstance } from "fastify"
 import { prisma } from "../lib/prisma"
+import dayjs from 'dayjs'
+
+import { z } from 'zod'
 
 export async function setupRoutes(app: FastifyInstance) {
     app.get('/habits', async () => {
         const habits = await prisma.habit.findMany()
         return habits
+    })
+
+    app.post('/habits', async (request) => {
+        const createHabitBody = z.object({
+            title: z.string(),
+            weekDays: z.array(z.number().min(0).max(6))
+        })
+
+        const { title, weekDays } = createHabitBody.parse(request.body)
+
+        const today = dayjs().startOf('day').toDate()
+
+        await prisma.habit.create({
+            data: {
+                title,
+                weekDays: {
+                    create: weekDays.map((weekDay) => {
+                        return {
+                            week_day: weekDay
+                        }
+                    })
+                },
+                created_at: today
+            }
+        })
     })
 }
