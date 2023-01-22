4136363a16dde5e51ebec481923b11785b463d6d
Paula Rolim <rolim.paula1999@gmail.com>
2023-01-22 19:20:21 -0300

    feat: web - habit background condition

--- web/src/pages/Home/components/SummaryTable/SummaryTable.tsx
+++ web/src/pages/Home/components/SummaryTable/SummaryTable.tsx
@@ -25,7 +25,7 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((summaryDate) => (
-          <Habit key={summaryDate.toISOString()} completed={0} />
+          <Habit key={summaryDate.toISOString()} completed={Math.random() * 5} amount={5} />
         ))}
 
         {amountOfDaysToFill > 0 &&
