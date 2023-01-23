52552fe3017ebbd89cd4d8d35935b5fb5f32ac1c
Paula Rolim <rolim.paula1999@gmail.com>
2023-01-22 23:08:04 -0300

    feat: mobile - create habit

--- mobile/src/screens/HabitForm/HabitForm.tsx
+++ mobile/src/screens/HabitForm/HabitForm.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import colors from "tailwindcss/colors";
 import { BackButton, CheckBox } from "../../components";
 import { Feather } from "@expo/vector-icons";
+import { httpClient } from "../../services/HttpClient";
 
 const availableWeekDays = [
   "Domingo",
@@ -16,14 +17,35 @@ const availableWeekDays = [
 ];
 
 export function HabitForm() {
-  const [selected, setSelected] = useState<number[]>([]);
+  const [title, setTitle] = useState("");
+  const [weekDays, setWeekDays] = useState<number[]>([]);
 
   const handleToggleWeekDays = (index: number) => {
-    if (selected.includes(index)) {
-      setSelected((prev) => prev.filter((weekDay) => weekDay !== index));
+    if (weekDays.includes(index)) {
+      setWeekDays((prev) => prev.filter((weekDay) => weekDay !== index));
     } else {
-      setSelected((prev) => [...prev, index]);
+      setWeekDays((prev) => [...prev, index]);
     }
+  };
+
+  const handleCreateHabit = async () => {
+    if (!title.trim() || weekDays.length === 0) return;
+
+    await httpClient
+      .post("/habits", {
+        title,
+        weekDays,
+      })
+      .then(() => {
+        setTitle("");
+        setWeekDays([]);
+
+        Alert.alert(`Hábito '${title}' criado com sucesso`);
+      })
+      .catch((error) => {
+        console.error("Something went wrong");
+        console.error(error);
+      });
   };
 
   return (
@@ -38,6 +60,8 @@ export function HabitForm() {
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white border-2 border-zinc-700 focus:border-green-600"
           placeholder="Exercícios, dormir bem, etc..."
           placeholderTextColor={colors.zinc[400]}
+          value={title}
+          onChangeText={setTitle}
         />
 
         <Text className="mt-4 text-white font-extrabold text-base mb-3">Qual a recorrência?</Text>
@@ -46,11 +70,14 @@ export function HabitForm() {
             key={weekDay}
             label={weekDay}
             onPress={() => handleToggleWeekDays(index)}
-            checked={selected.includes(index)}
+            checked={weekDays.includes(index)}
           />
         ))}
 
-        <TouchableOpacity className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6">
+        <TouchableOpacity
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          onPress={handleCreateHabit}
+        >
           <Feather name="check" size={20} color={colors.white} />
           <Text className="text-white font-semibold text-base ml-3">Confirmar</Text>
         </TouchableOpacity>
