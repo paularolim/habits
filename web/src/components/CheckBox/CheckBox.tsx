22cdd6a29c3d5021f4b78688e5497b86166d88b7
Paula Rolim <rolim.paula1999@gmail.com>
2023-01-22 22:05:39 -0300

    fix: web - checkbox typo

--- web/src/components/CheckBox/CheckBox.tsx
+++ web/src/components/CheckBox/CheckBox.tsx
@@ -3,9 +3,9 @@ import { Check } from "phosphor-react";
 
 import { CheckBoxProps } from "./types";
 
-export function CheckBox({ label }: CheckBoxProps) {
+export function CheckBox({ label, ...rest }: CheckBoxProps) {
   return (
-    <Checkbox.Root className="flex items-center gap-3 group">
+    <Checkbox.Root className="flex items-center gap-3 group" {...rest}>
       <div className="w-8 h-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
         <Checkbox.Indicator>
           <Check size={20} className="text-white" />
--- web/src/components/CheckBox/types.ts
+++ web/src/components/CheckBox/types.ts
@@ -1,3 +1,5 @@
-export interface CheckBoxProps {
+import { CheckboxProps } from "@radix-ui/react-checkbox";
+
+export interface CheckBoxProps extends CheckboxProps {
     label: string
 }
