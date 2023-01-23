f47ffc01a58d88b0cecd4a959700e63352dba7bc
Paula Rolim <rolim.paula1999@gmail.com>
2023-01-22 22:06:47 -0300

    feat: web - show checkbox on form

--- web/src/pages/Home/components/HabitForm/HabitForm.tsx
+++ web/src/pages/Home/components/HabitForm/HabitForm.tsx
@@ -1,8 +1,37 @@
 import { Check } from "phosphor-react";
+import { FormEvent, useCallback, useState } from "react";
+import { CheckBox } from "../../../../components";
+
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
 
 export function HabitForm() {
+  const [title, setTitle] = useState("");
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  const handleToggleWeekDay = useCallback((index: number) => {
+    if (weekDays.includes(index)) {
+      setWeekDays((prev) => prev.filter((item) => item !== index));
+    } else {
+      setWeekDays((prev) => [...prev, index]);
+    }
+  }, []);
+
+  const handleCreateHabit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("title", title);
+    console.log("weekDays", weekDays);
+  };
+
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form className="w-full flex flex-col mt-6" onSubmit={handleCreateHabit}>
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual seu comprometimento?
       </label>
@@ -13,11 +42,18 @@ export function HabitForm() {
         placeholder="Exercícios, dormir bem, etc..."
         autoFocus
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        onChange={(event) => setTitle(event.target.value)}
       />
 
       <label htmlFor="title" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
+
+      {availableWeekDays.map((day, index) => (
+        <div key={day} className="mt-2 flex flex-col gap-3">
+          <CheckBox label={day} onCheckedChange={() => handleToggleWeekDay(index)} />
+        </div>
+      ))}
 
       <button
         type="submit"
